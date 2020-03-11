a7e148fed25ffbfabd9719604469f155643597be
Bülent AY <bulent_437@hotmail.com>
2020-03-11 21:12:53 +0300

    temiz bir sayfa

--- SeyruSefer/SeyruSefer/Form1.Designer.cs
+++ SeyruSefer/SeyruSefer/Form1.Designer.cs
@@ -28,34 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // simpleButton1
-            // 
-            this.simpleButton1.Location = new System.Drawing.Point(237, 76);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
-            this.simpleButton1.TabIndex = 0;
-            this.simpleButton1.Text = "simpleButton1";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(400, 100);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 278);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.simpleButton1);
+            this.ClientSize = new System.Drawing.Size(600, 246);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -63,9 +42,6 @@
         }
 
         #endregion
-
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
