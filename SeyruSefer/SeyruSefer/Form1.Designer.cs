48cd7a54bb334d0bea3c6b1d5766f1bf5d2636e6
cüneyt yazici <cuuneyt_yazicii@hotmail.com>
2020-03-11 21:05:01 +0300

    iki

--- SeyruSefer/SeyruSefer/Form1.Designer.cs
+++ SeyruSefer/SeyruSefer/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // simpleButton1
@@ -39,11 +40,21 @@
             this.simpleButton1.TabIndex = 0;
             this.simpleButton1.Text = "simpleButton1";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 278);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.simpleButton1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,6 +65,7 @@
         #endregion
 
         private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
